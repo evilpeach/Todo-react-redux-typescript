082129098ac72c10c1620e29c097aec2ef9a375d
Kanisorn Thong <kanisorn.peach@gmail.com>
2018-10-26 16:49:23 +0700

    update code spitting using react-loadable

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
-import TodoList from '../containers/TodoList';
-import TodoOptions from '../containers/TodoOptions';
-import TodoForm from '../containers/TodoForm';
+// import TodoList from '../containers/TodoList';
+// import TodoOptions from '../containers/TodoOptions';
+// import TodoForm from '../containers/TodoForm';
+import Loadable from 'react-loadable';
+
+const TodoForm = Loadable({
+    loader: () => import(/* webpackChunkName: "todo.form" */'../containers/TodoForm'),
+    loading: () => <div>loading Todoform ...</div>
+});
+
+const TodoList = Loadable({
+    loader: () => import(/* webpackChunkName: "todo.list" */'../containers/TodoList'),
+    loading: () => <div>loading Todolist ...</div>
+})
+
+const TodoOptions = Loadable({
+    loader: () => import(/* webpackChunkName: "todo.options" */'../containers/TodoOptions'),
+    loading: () => <div> loaing TodoOptions ...</div>
+})
 
 export default class App extends React.Component {
+
     render() {
         const bgStyle = {
             margin: 'auto',
